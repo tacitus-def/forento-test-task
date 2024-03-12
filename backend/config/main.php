a97daa3ff4f41b4d237c46df7ecda7559add8900
Petr Sleptsov <spetr@bk.ru>
2024-03-12 12:04:10 +0900

    Настройка RBAC

--- backend/config/main.php
+++ backend/config/main.php
@@ -21,6 +21,9 @@ return [
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'session' => [
             // this is the name of the session cookie used for login on the backend
             'name' => 'advanced-backend',
