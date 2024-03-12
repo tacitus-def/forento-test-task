350ab05ca1d1929cfc60f68cac6af431f1717bf3
Petr Sleptsov <spetr@bk.ru>
2024-03-12 18:50:48 +0900

    Добавлены ссылки на Clients и Users в главное меню

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -37,9 +37,13 @@ AppAsset::register($this);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
     ];
+    if (!Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Users', 'url' => ['/user/index']];
+        $menuItems[] = ['label' => 'Clients', 'url' => ['/client/index']];
+    }
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    }     
+    }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
         'items' => $menuItems,
